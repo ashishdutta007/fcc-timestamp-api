7ac70cd2f47ab2293fb0a83a5041338e4ebf9f07
Ashish <ashish.dutta050@gmail.com>
2016-12-27 19:01:21 +0000

    final

--- server.js
+++ server.js
@@ -1,14 +1,69 @@
+//Using express web http framework
 var express = require("express");
+//Using moment module
+var moment = require("moment");
 var app = express();
 
-//binding a middleware layer to server object
-app.use(function(req, res) {
-	res.write('Hello World!', 'utf8', function(err) {
-		if (err) {
-			console.log(err);
-		}
+//Binding middleware to server object to recieve request to the path 
+app.use('/:dateStr', function(request, response) {
+	request.on('error', function(error) {
+		return console.log("Improper request, error occured : ", error);
 	});
-	res.end();
+	response.on('error', function(error) {
+		return console.log("Error occured in sending response: ", error);
+	});
+
+	var dateStr = request.params.dateStr.toString();
+
+	//check dateStr wheather is not-A-number
+	if (!isNaN(dateStr)) {
+
+		var time = Number(dateStr);
+
+		if (moment.unix(time).isValid()) {
+			var naturalDate = moment.unix(time);
+			//Create a natural date out of unix timestamp
+			var unixTime = moment(time).format("X");
+
+			response.json({
+				"unix": unixTime,
+				"natural": naturalDate
+			});
+			response.end();
+		}
+		else {
+			response.json({
+				"unix": null,
+				"natural": null
+			});
+			response.end();
+		}
+
+	}
+	else {
+		if (moment(dateStr, "MM D, YYYY").isValid()) {
+			var naturalDate = moment(dateStr, "MM D, YYYY");
+			//Create a natural date out of unix timestamp
+			var unixTime = moment(naturalDate).format("X");
+
+			response.json({
+				"unix": unixTime,
+				"natural": naturalDate
+			});
+			response.end();
+		}
+		else {
+			response.json({
+				"unix": null,
+				"natural": null
+			});
+			response.end();
+		}
+	}
 });
 
-app.listen(Number(8080));
+//moment is a wrapper around the 'Date' object
+/*var d = new Date('6 jan 1990 12:13:13 UTC');
+console.log(moment(d, "DD MM YYYY"));
+console.log(moment(d).isValid());
+*/
